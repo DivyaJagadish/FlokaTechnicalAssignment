4c914dfd8f151190dc940fa5aaf6605665ca942c
Divya Jagadish <divyajagadish21@gmail.com>
2021-06-01 00:29:53 +0000

    Axios request to API

--- server.js
+++ server.js
@@ -7,10 +7,22 @@ const PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+// Axios request to get data from API
+
+let templatevar = {};
+axios
+	.get("http://jsonplaceholder.typicode.com/users")
+	.then((result) => {
+		templatevar["result"] = result.data;
+		console.log(templatevar);
+	})
+	.catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
 	res.render("welcome");
 });
 
+// Server listening in PORT 8080
 app.listen(PORT, () => {
 	console.log(`Example app listening on port ${PORT}`);
 });
