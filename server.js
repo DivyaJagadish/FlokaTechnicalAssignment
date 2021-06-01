0527e42177518257aacd63449c305655b6a05327
Divya Jagadish <divyajagadish21@gmail.com>
2021-06-01 00:23:27 +0000

    Create views and ejs welcome page

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+	res.render("welcome");
+});
 
 app.listen(PORT, () => {
 	console.log(`Example app listening on port ${PORT}`);
